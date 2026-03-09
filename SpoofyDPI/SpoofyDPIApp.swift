b3f2128f5f07d2289532fcffdc6a679d0b4a8e2f
rnglol <createsite108@gmail.com>
2026-03-09 19:22:46 +0300

    Initial Commit

---
+++ SpoofyDPI/SpoofyDPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoofyDPIApp.swift
+//  SpoofyDPI
+//
+//  Created by valeriy-mcbook on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpoofyDPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
